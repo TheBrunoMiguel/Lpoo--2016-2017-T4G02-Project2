e3e03f09c8171dbe22e3e7d2d120a43eeb093210
Bruno Miguel <up201504781@gcloud.fe.up.pt>
2017-05-02 17:10:04 +0100

    initial upload
    Uploaded files directly, couldnt do ti any other way

---
+++ LpooProject2/desktop/src/com/lpoot4g02/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lpoot4g02.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lpoot4g02.game.RpgGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RpgGame(), config);
+	}
+}
